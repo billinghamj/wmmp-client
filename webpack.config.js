0d5f02575602d067d40ba92377b315efed276e59
James Billingham <git@jamesbillingham.com>
2018-04-29 19:17:50 +0100

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+/* eslint-env node */
+
+const path = require('path');
+
+module.exports = {
+	entry: './src/index.js',
+	output: {
+		filename: 'bundle.js',
+		path: path.resolve(__dirname, 'dist'),
+	},
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /(node_modules|bower_components)/,
+				use: {
+					loader: 'babel-loader',
+					options: {
+						presets: ['@babel/preset-env'],
+						plugins: ['@babel/plugin-proposal-class-properties'],
+					},
+				},
+			},
+		],
+	},
+	stats: {
+		colors: true,
+	},
+	devtool: 'source-map',
+};
